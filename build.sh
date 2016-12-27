1cf25130400964a84821e6da1b1598e8f25b522e
Chandler Newman <chandler2newman@hotmail.co.uk>
2016-12-27 20:43:49 +0000

    Added basic configs with amd64 + i386 changes

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ cd ubuntu-kernel
 patch -p1 < ../patches/vfio/0001-i915-Add-module-option-to-support-VGA-arbiter-on-HD-.patch
 patch -p1 < ../patches/vfio/0001-pci-Enable-overrides-for-missing-ACS-capabilities-4..patch
 
+#Configs
+cp -r -f -v config/** ubuntu-kernel/debian.master/config/
+
 #Build
 fakeroot debian/rules clean
 fakeroot debian/rules binary-headers binary-generic binary-perarch
