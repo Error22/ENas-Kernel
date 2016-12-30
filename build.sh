5181da39cd53e98a4c4ce9b0a9e48120dfc4a33b
Chandler Newman <chandler2newman@hotmail.co.uk>
2016-12-30 18:33:21 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -3,19 +3,18 @@
 cd ubuntu-kernel
 
 #VFIO patches
+echo Applying patches
 patch -p1 < ../patches/vfio/0001-i915-Add-module-option-to-support-VGA-arbiter-on-HD-.patch
 patch -p1 < ../patches/vfio/0001-pci-Enable-overrides-for-missing-ACS-capabilities-4..patch
 
 #Configs
+echo Copying configs
 cp -r -f -v config/** ubuntu-kernel/debian.master/config/
 
-#Version
-cd debian.master
-(sed 0,/\)/{s/\)/+enas-1.0\)/} changelog ) > changelog.temp
-cp changelog.temp changelog
-rm changelog.temp
-cd ../
-
 #Build
+echo Building
 fakeroot debian/rules clean
-fakeroot debian/rules binary-headers binary-generic binary-perarch
+fakeroot make-kpkg -j N --initrd --append-to-version=enas-1.0 kernel-image kernel-headers
+#fakeroot debian/rules binary-headers binary-generic binary-perarch
+
+echo Done!
