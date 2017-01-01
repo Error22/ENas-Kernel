062ce87d432e3d507be9a18b1ea4d03d1c9cb2d7
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-01-01 13:51:35 +0000

    Added basic build and core count settings

--- build.sh
+++ build.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+#---- Settings ----
+#Version
+if [ -z ${BUILD_NUMBER+x} ]; then
+	echo Build number found!
+	version=-enas-1.$BUILD_NUMBER
+else
+	echo No build number, custom build
+	version=-enas-1-custom
+fi
+
+#Core count
+if [ -z ${CORE_COUNT+x} ]; then
+	echo Core count found
+	corecount=$CORE_COUNT
+else
+	echo Looking up core count
+	corecount=$(grep -c ^processor /proc/cpuinfo)
+fi
+
+#---- Body ----
 cd ubuntu-kernel
 
 #VFIO patches
@@ -14,7 +34,6 @@ cp -r -f -v config/** ubuntu-kernel/debian.master/config/
 #Build
 echo Build
 fakeroot debian/rules clean
-corecount=$(grep -c ^processor /proc/cpuinfo)
-fakeroot make -j$corecount olddefconfig bindeb-pkg LOCALVERSION=-enas-1
+fakeroot make -j$corecount olddefconfig bindeb-pkg LOCALVERSION=$version
 
 echo Done!
